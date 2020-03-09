220951503333627cb8bc40f9eccbebbe3de8e26f
lwjj <848565635@qq.com>
2020-03-09 17:50:39 +0800

    :bug: 新增商品列表接口

--- src/main/java/com/next/wxsell/controller/BuyerProductController.java
+++ src/main/java/com/next/wxsell/controller/BuyerProductController.java
@@ -1,9 +1,23 @@
 package com.next.wxsell.controller;
 
+import com.next.wxsell.VO.ProductInfoVO;
+import com.next.wxsell.VO.ProductVO;
 import com.next.wxsell.VO.ResultVO;
+import com.next.wxsell.dataobject.ProductCategory;
+import com.next.wxsell.dataobject.ProductInfo;
+import com.next.wxsell.servier.CategoryService;
+import com.next.wxsell.servier.ProductService;
+import com.next.wxsell.utils.ResultVOUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author lwj
@@ -13,9 +27,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/list")
-    public ResultVO list() {
-        ResultVO resultVO = new ResultVO();
-        return resultVO;
+    public ResultVO list(@RequestParam(value = "sellerId",required = false) String sellerId) {
+        //1.查询所有的上架商品
+        List<ProductInfo> productInfoList = productService.findUpAll();
+
+        //2.查询类目
+        List<Integer> categoryTypeList = productInfoList.stream()
+                .map(e -> e.getCategoryType())
+                .collect(Collectors.toList());
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
+
+        //3.数据拼装
+        List<ProductVO> productVOList = new ArrayList<>();
+        for(ProductCategory productCategory: productCategoryList) {
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for(ProductInfo productInfo: productInfoList) {
+                if(productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo,productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
+
+        return ResultVOUtil.success(productVOList);
     }
 }
